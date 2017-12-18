20293a12a7d15eca4ec12b938ebb00e78ac4ff8c
bnesposito <bnesposito2@gmail.com>
2017-12-18 15:59:53 -0500

    Add brainstorm ideas for preprocessing and modeling

--- code/main.py
+++ code/main.py
@@ -7,13 +7,51 @@ import work_data
 import models
 
 def main():
-    logger = config.ConfigLogger(__name__, 10)
-    t0 = time.ctime()
-    logger.info('OA')
+    logger = config.config_logger(__name__, 10)
+    t0 = time.time()
+
+    train_client_path = './data/raw/csv/train_clientes.csv'
+    train_reque_path = './data/raw/csv/train_requerimientos.csv'
+    test_client_path = './data/raw/csv/test_clientes.csv'
+    test_reque_path = './data/raw/csv/test_requerimientos.csv'
+
+    logger.info('Beginning execution')
+    logger.info('Load dataframes')
+    main_client = work_data.load_csv_to_df(train_client_path)
+    main_reque = work_data.load_csv_to_df(train_reque_path)
+
+    work_data.basic_descriptive(main_client)
+    work_data.basic_descriptive(main_reque)
+    print(main_client.head().to_string())
+    print(main_reque.head().to_string())
+
+    print(main_client['ATTRITION'].value_counts())
+    #TODO Unbalanced classification problem
+
+    # Brain storm for preprocessing:
+    # 1. As you see it.
+    # 2. Mean across the 5 months.
+    # 3. Weighted mean across the 5 months. Give more weight to the most recent.
+    # 4. PCA. -> visualize 2 dim!
+    # 5. 2-month window average.
+    # 6. Discretize continuous variables. Especially those with many zeros.
+
+    # Fist, create the classification algorithm, then implement the preprocessing alternatives
+
+    # Brain storm for classification:
+    # 1. GBM
+    # 2. XGBoost
+    # 3. Adaboost
+    # 4. SMOTE
+    # 5. Boosting for unbalanced classes.
+    # 6. Logit lasso/ridge
+    # 7. NN -> problem optimizing
+    # 8. Voting Classifier - soft (for the best 3?)
+
+    # All the algorithms must be implemented with a 10-fold CV and a GridSearch.
 
     config.time_taken_display(t0)
 
 
-
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
